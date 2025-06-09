f419c53ab7ea07864a5458b432a5372d3bf8012b
Free-cpu2 <firdavsbek347@gmail.com>
2025-06-09 18:24:38 +0500

    asdvasdvsadv

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,23 @@ function App() {
       <p>
         hello botlar
       </p>
+      <section className="manage-community">
+        <h2>Manage your entire community in a single system</h2>
+        <div className="features">
+          <div className="feature-box">
+            <h3>Membership Organisations</h3>
+            <p>Our membership management software provides full automation of membership renewals and payments</p>
+          </div>
+          <div className="feature-box">
+            <h3>National Associations</h3>
+            <p>Our membership management software provides full automation of membership renewals and payments</p>
+          </div>
+          <div className="feature-box">
+            <h3>Clubs And Groups</h3>
+            <p>Our membership management software provides full automation of membership renewals and payments</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
