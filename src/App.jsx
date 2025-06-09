5213c82500b86e573551a9d86590ad9018bc8a3a
Stich-cyber <maradam679@gmail.com>
2025-06-09 18:33:20 +0500

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p>Lorem ipsum dolor sit.</p>
-      <p>hello botlar</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-        dolores, reiciendis nam dignissimos suscipit unde deserunt dolor illo
-        voluptatibus harum sequi ea dolorum corrupti perspiciatis error magnam
-        nobis itaque blanditiis?
-      </p>
-      <p>
-        hello botlar
-      </p>
-      <p>Lorem, ipsum dolor.</p>
-      <p>Lorem, ipsum dolor.</p>
-      <p>Lorem, ipsum dolor.</p>
-      <p>Lorem, ipsum dolor.</p>
     </>
   );
 }
